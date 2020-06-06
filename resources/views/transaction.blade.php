f204eb99bde80bb6aac8fb551b1f47ab559a7e46
Okoye@Madara <okoyep98@gmail.com>
2020-06-06 13:25:07 +0100

    transaction page complete

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -1,1 +1,323 @@
 @extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/transaction.css') }}">
+@endsection
+@section('content')
+    <div class="row">
+        <div class="container-fluid">
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+            <div class="card card-info shadow-sm">
+                <div class="card-head">
+                    <h4>products name</h4>
+                </div>
+                <div class="card-body card-type-3">
+                    <img src="{{asset('image/products/mouse_computer_green_82454_1280x720.jpg')}}" alt="">
+                    <div>
+                        <div class="pricing-details">
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1 user agent</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">Core features</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">1GB storage</div>
+                            </div>
+                            <div class="pricing-item">
+                                <div class="pricing-item-icon"><i class="fa fa-check"></i></div>
+                                <div class="pricing-item-label">2 Custom domain</div>
+                            </div>
+                            <div class="pricing-item">
+                                <span class="text-muted" style="font-size: 16px">price:</span> <strong class="text-info ml-2"><i>$9.99</i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div>
+                        <small class="text-muted">status:</small> <strong class="text-success ml-2">success</strong>
+                    </div>
+                    <div>
+                        <small class="text-muted">date:</small> <strong class="text-danger ml-2">Jan 07, 2020.</strong>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </div>
+@endsection
